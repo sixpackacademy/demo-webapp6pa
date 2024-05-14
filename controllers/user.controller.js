d8978620ebd7a4a584fc9ed6a4cd3f37aff05d0b
Tanjil <tanjilkh@gmail.com>
2024-05-14 17:19:28 +0100

    register controller validate claimed username

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,9 +2,29 @@ const models = require('../models');
 const User = models.User;
 
 const register = async(req, res) => {
-    const data = req.body;
-    const user = await User.create(data);
-    res.json(user);
+    try{
+        // Get data from body
+        const data = req.body;
+
+        // Try to find a user
+        // users can't register
+        // with usernames that already exists
+        const user = await User.findOne({where: {username: data.username}})
+        if(!user){
+            // No user claimed that username, so it creates a user with that username
+            const created_user = await User.create(data);
+
+            // Send response as created user
+            res.json(created_user);
+        } else {
+            // If a user tries to get a claimed username
+            // It will return a warning message that is already claimed
+            res.json({message: "Username already claimed."})
+        }
+    } catch(error) {
+        res.json({message: error})
+    }
+    
 }
 
 const login = async(req, res) => {
