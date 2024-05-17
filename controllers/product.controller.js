910cab367dbe44bbf32457beb47128f595a8c903
Tanjil <tanjilkh@gmail.com>
2024-05-17 09:16:06 +0100

    try catch statement

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,13 +2,19 @@ const models = require('../models');
 const Product = models.Product;
 
 const create = async(req, res) => {
-    const data = req.body;
-    const product_exists = await Product.findOne({where: {name: data.name}})
-    if(product_exists){
-        res.json({message: 'product already exists.'})
-    } else {
-        const product = await Product.create(data);
-        res.json(product);
+    try {
+        const data = req.body;
+        const product_exists = await Product.findOne({where: {name: data.name}})
+        if(product_exists){
+            res.json({message: 'product already exists.'})
+        } else {
+            const product = await Product.create(data);
+            res.json(product);
+        } 
+    } 
+    
+    catch (error) {
+        res.json({message: error})
     }
     
 }
