ec68386b74e039303b1af14ff1f5d8d6f62d9d48
Tanjil <tanjilkh@gmail.com>
2024-05-17 09:09:11 +0100

    validate creation product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,8 +3,14 @@ const Product = models.Product;
 
 const create = async(req, res) => {
     const data = req.body;
-    const product = await Product.create(data);
-    res.json(product);
+    const product_exists = await Product.findOne({where: {name: data.name}})
+    if(product_exists){
+        res.json({message: 'product already exists.'})
+    } else {
+        const product = await Product.create(data);
+        res.json(product);
+    }
+    
 }
 
 module.exports = {
